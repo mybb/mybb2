518a816d36ae3dd9668be19fec3b29115b4204f2
ATofighi <alirtofighim@gmail.com>
2015-02-13 15:09:35 +0330

    Start Localization

---
+++ resources/lang/en/member.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+	'login' => 'Log In',
+	'signup' => 'Sign Up',
+	'UsernameOrEmail' => 'Username/Email',
+	'password' => 'Password',
+	'forgotpassword' => 'Forgotten your password?',
+
+];
