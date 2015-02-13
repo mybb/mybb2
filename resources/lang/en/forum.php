975022754218d60ecff49b7fdd8cbb60a2798126
ATofighi <alirtofighim@gmail.com>
2015-02-13 18:15:15 +0330

    Localization Forum.show

---
+++ resources/lang/en/forum.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'allforums' => 'All Forums',
+	'notfound' => 'No Forums Found!',
+	'childforums' => 'Child Forums',
+	'topics' => 'Topics',
+	'markread' => 'Mark Read',
+	'follow' => 'Follow',
+	'posttopic' => 'Post Topic',
+	'topictitle' => 'Topic Title',
+	'startdate' => 'Start Date',
+	'replies' => 'Replies',
+	'latestpost' => 'Latest Post',
+
+];
