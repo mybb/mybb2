3eba70fa9680ccb5c7b78894e30844b4f65c8263
ATofighi <alirtofighim@gmail.com>
2015-02-25 21:04:42 +0330

    add username style

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,8 @@ class CreateUsersTable extends Migration
 			$table->string('avatar')->default('');
 			$table->string('dob', 10)->default('1-1-');
 			$table->string('usertitle')->default('');
+			$table->integer('num_posts')->unsigned()->default(0);
+			$table->integer('num_topics')->unsigned()->default(0);
 			$table->nullableTimestamps();
 		});
 	}
