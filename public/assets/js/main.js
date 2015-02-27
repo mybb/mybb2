4f6aafb504526db66da7704b643bdb417b779110
Paul Hedman <paul@mybb.com>
2015-02-26 19:38:50 -0600

    Adding WORKING modals! Woohoo!

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,19 +1,69 @@
-function modal(page, find) {
-	if(page[0] == '/')
-		page = page.substr(1);
+(function($, window) {
+    window.MyBB = window.MyBB || {};
+    
+	window.MyBB.Modals = function Modals()
+	{
+		$("*[data-modal]").on("click", this.toggleModal).bind(this);
+	};
 
-	$.get('/'+page, function(ans) {
-		// find is optional and defaults to '#content'
-		if(typeof find == 'undefined') { find = '#content'; }
+	window.MyBB.Modals.prototype.toggleModal = function toggleModal(event) {
+		event.preventDefault();
 
-		var obj = $(ans);
-		var html = $(find, obj).html();
-		$('<div class="modalDialog">'+html+'</div>').appendTo('body').modal();
-		$('.modalHide').hide();
-		$("input[type=number]").stepper();
-		$(".password-toggle").hideShowPassword(false, true);
-	});
-}
+		// Check to make sure we're clicking the link and not a child of the link
+		if(event.target.nodeName === "A")
+		{
+			// Woohoo, it's the link!
+			var modalOpener = event.target,
+				modalSelector = $(modalOpener).data("modal"),
+				modalFind = $(modalOpener).data("modal-find"),
+				modal = $('<div/>', {
+	    			"class": "modal-dialog",
+				}),
+				modalContent = "";
+		} else {
+			// Nope, it's one of those darn children.
+			var modalOpener = event.target,
+				modalSelector = $(modalOpener).parent().data("modal"),
+				modalFind = $(modalOpener).data("modal-find"),
+				modal = $('<div/>', {
+	    			"class": "modal-dialog",
+				}),
+				modalContent = "";
+		}
+
+		if (modalSelector.substring(0, 1) === "." || modalSelector.substring(0, 1) === "#") {
+			// Assume using a local, existing HTML element.
+			modalContent = $(modalSelector).html();
+			modal.html(modalContent);
+			modal.appendTo("body").modal();
+			$('.modalHide').hide();
+			$("input[type=number]").stepper();
+			$(".password-toggle").hideShowPassword(false, true);
+		} else {
+			// Assume modal content is coming from an AJAX request
+
+			// data-modal-find is optional, default to "#content"
+			if (modalFind === undefined) {
+				modalFind = "#content";
+			}
+
+			$.get('/'+modalSelector, function(response) {
+				var responseObject = $(response);
+
+				modalContent = $(modalFind, responseObject).html();
+				modal.html(modalContent);
+				modal.appendTo("body").modal({
+    fadeDuration: 250
+  });
+				$('.modalHide').hide();
+				$("input[type=number]").stepper();
+				$(".password-toggle").hideShowPassword(false, true);
+			});
+		}
+	};
+    
+    var modals = new window.MyBB.Modals(); // TODO: put this elsewhere :)
+})(jQuery, window);
 $('html').addClass('js');
 
 $(function () {
