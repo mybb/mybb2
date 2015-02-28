b13c8c50431abb1d9e75eb427a176a06f0942d6c
faviouz <fabiu.maia@gmail.com>
2015-02-28 02:04:39 +0000

    add topic api transformer

---
+++ app/Transformers/TopicTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MyBB\Core\Transformers;
+
+use MyBB\Core\Database\Models\Topic;
+use League\Fractal\TransformerAbstract;
+
+class TopicTransformer extends TransformerAbstract
+{
+    public function transform(Topic $topic)
+    {
+        return [
+            'title'         => $topic->title,
+            'slug'          => $topic->slug,
+            'forum_id'      => (int) $topic->forum_id,
+            'user_id'       => (int) $topic->user_id,
+            'first_post_id' => (int) $topic->first_post_id,
+            'last_post_id'  => (int) $topic->last_post_id,
+            'views'         => (int) $topic->views,
+            'created_at'    => (string) $topic->created_at,
+            'updated_at'    => (string) $topic->updated_at,
+            'num_posts'     => (int) $topic->num_posts,
+        ];
+    }
+}
