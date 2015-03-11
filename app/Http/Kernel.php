e8ab9a53bb899d2e25a115967b6b1f6259befde1
Euan T <euantorano@gmail.com>
2015-03-11 22:08:52 +0000

    Added settings middleware to Kernel.
    Signed-off-by: Euan T <euantorano@gmail.com>

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -17,6 +17,7 @@ class Kernel extends HttpKernel
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
 		'Illuminate\Foundation\Http\Middleware\VerifyCsrfToken',
+	    'MyBB\Settings\Middleware\SaveSettingsOnTerminate',
 	];
 
 	/**
