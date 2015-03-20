aea0a79333d15e1a1d8e7f08710fbcbd17cde069
ATofighi <alirtofighim@gmail.com>
2015-03-20 19:33:23 +0330

    /me and /slap for quote

--- app/Renderers/Post/Quote.php
+++ app/Renderers/Post/Quote.php
@@ -13,7 +13,18 @@ class Quote {
 	public function renderFromPost(Post $post)
 	{
 		$post = new PostPresenter($post);
+		$message = $post->content;
+		$slapUsername = $post->author->name;
+		$message = preg_replace('#(>|^|\r|\n)/me ([^\r\n<]*)#i',
+			"\\1* {$slapUsername} \\2", $message);
+		$slap = trans('parser::parser.slap');
+		$withTrout = trans('parser::parser.withTrout');
+		$message = preg_replace('#(>|^|\r|\n)/slap ([^\r\n<]*)#i',
+			"\\1* {$slapUsername} {$slap} \\2 {$withTrout}",
+			$message);
+		$message = preg_replace("#\[attachment=([0-9]+?)\]#i", '', $message);
+
 		return "[quote='".e($post->author->name)."' pid='{$post->id}' dateline='".
-				$post->created_at->getTimestamp()."']\n{$post->content}\n[/quote]";
+				$post->created_at->getTimestamp()."']\n{$message}\n[/quote]";
 	}
 }
