22481593fa61966806766e89b91114f33bee341f
JN-Jones <Jones_h1@web.de>
2015-04-08 17:55:53 +0200

    Move soft deleting to TopicDeleter and use that instead of the repository

--- app/Services/TopicDeleter.php
+++ app/Services/TopicDeleter.php
@@ -2,39 +2,64 @@
 
 namespace MyBB\Core\Services;
 
-use MyBB\Core\Database\Repositories\IPostRepository;
+use MyBB\Core\Database\Repositories\ForumRepositoryInterface;
+use MyBB\Core\Database\Repositories\PostRepositoryInterface;
 use MyBB\Core\Database\Models\Topic;
 
 class TopicDeleter
 {
-	/** @var IPostRepository $postRepository */
+	/** @var PostRepositoryInterface $postRepository */
 	private $postRepository;
 
+	/** @var ForumRepositoryInterface $forumRepository */
+	private $forumRepository;
+
 	/**
-	 * @param IPostRepository $postRepository
+	 * @param PostRepositoryInterface  $postRepository
+	 * @param ForumRepositoryInterface $forumRepository
 	 */
-	public function __construct(IPostRepository $postRepository)
+	public function __construct(PostRepositoryInterface $postRepository, ForumRepositoryInterface $forumRepository)
 	{
 		$this->postRepository = $postRepository;
+		$this->forumRepository = $forumRepository;
 	}
 
 	/**
 	 * @param Topic $topic
 	 *
-	 * @return mixed
+	 * @return bool
 	 */
 	public function deleteTopic(Topic $topic)
 	{
-		// First we need to remove old foreign keys - otherwise we can't delete posts
-		$topic->update([
-			'first_post_id' => null,
-			'last_post_id' => null
-		]);
+		if ($topic->deleted_at == null) {
+			$topic->forum->decrement('num_topics');
+			$topic->forum->decrement('num_posts', $topic->num_posts);
 
-		// Now delete the posts for this topic
-		$this->postRepository->deletePostsForTopic($topic);
+			$topic->author->decrement('num_topics');
 
-		// And finally delete the topic
-		return $topic->forceDelete();
+			$success = $topic->delete();
+
+			if ($success) {
+				if ($topic->last_post_id == $topic->forum->last_post_id) {
+					$this->forumRepository->updateLastPost($topic->forum);
+				}
+			}
+
+			return $success;
+		} else {
+			// First we need to remove old foreign keys - otherwise we can't delete posts
+			$topic->update([
+				'first_post_id' => null,
+				'last_post_id' => null
+			]);
+
+			// Now delete the posts for this topic
+			$this->postRepository->deletePostsForTopic($topic);
+
+			// And finally delete the topic
+			$topic->forceDelete();
+		}
+
+		return true;
 	}
 }
