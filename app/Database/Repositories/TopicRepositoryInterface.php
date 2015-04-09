596d500354e5cd2df9d83221758dc27fb78cc3a6
JN-Jones <jones_h1@web.de>
2015-04-09 11:37:35 +0200

    Remove 'deleteTopic' from interface as done in #36

--- app/Database/Repositories/TopicRepositoryInterface.php
+++ app/Database/Repositories/TopicRepositoryInterface.php
@@ -69,7 +69,7 @@ interface TopicRepositoryInterface
 	 * Find a single topic with a specific slug and ID.
 	 *
 	 * @param string $slug The slug for the topic.
-	 * @param int    $id   The ID of the topic to find.
+	 * @param int $id The ID of the topic to find.
 	 *
 	 * @return mixed
 	 */
@@ -94,21 +94,12 @@ interface TopicRepositoryInterface
 	/**
 	 * Edit a topic
 	 *
-	 * @param Topic $topic        The topic to edit
+	 * @param Topic $topic The topic to edit
 	 * @param array $topicDetails The details of the post to add.
 	 *
 	 * @return mixed
 	 */
 	public function editTopic(Topic $topic, array $topicDetails);
-
-	/**
-	 * Delete a topic
-	 *
-	 * @param Topic $topic The topic to delete
-	 *
-	 * @return mixed
-	 */
-	public function deleteTopic(Topic $topic);
 
 	/**
 	 * Restore a topic
