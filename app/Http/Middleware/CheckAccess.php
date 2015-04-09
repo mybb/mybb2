4b52cd7e27231d304ba5470b4cbee9a5032f77bc
JN-Jones <jones_h1@web.de>
2015-04-09 20:00:09 +0200

    Fix missing initialization on CheckAccess middleware

--- app/Http/Middleware/CheckAccess.php
+++ app/Http/Middleware/CheckAccess.php
@@ -3,6 +3,7 @@
 use Closure;
 use MyBB\Auth\Contracts\Guard;
 use MyBB\Core\Permissions\PermissionChecker;
+use MyBB\Settings\Store;
 
 class CheckAccess
 {
@@ -12,14 +13,19 @@ class CheckAccess
 	/** @var  PermissionChecker */
 	private $permissionChecker;
 
+	/** @var Store */
+	private $settings;
+
 	/**
 	 * @param Guard             $auth
 	 * @param PermissionChecker $permissionChecker
+	 * @param Store             $settings
 	 */
-	public function __construct(Guard $auth, PermissionChecker $permissionChecker)
+	public function __construct(Guard $auth, PermissionChecker $permissionChecker, Store $settings)
 	{
 		$this->auth = $auth;
 		$this->permissionChecker = $permissionChecker;
+		$this->settings = $settings;
 	}
 
 	/**
@@ -36,8 +42,20 @@ class CheckAccess
 			return $next($request);
 		}
 
+		app()->setLocale($this->settings->get('user.language', 'en'));
+
+		$langDir = [
+			'left' => 'left',
+			'right' => 'right'
+		];
+
+		if (trans('general.direction') == 'rtl') {
+			$langDir['left'] = 'right';
+			$langDir['right'] = 'left';
+		}
+
 		// TODO: The acp should probably create another view
-		return view('errors.no_permission');
+		return view('errors.no_permission')->with('auth_user', $this->auth->user())->with('langDir', $langDir);
 	}
 
 	/**
