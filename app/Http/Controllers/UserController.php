4705002543a636a7a84c635e8491f6c43b4e518c
JN-Jones <jones_h1@web.de>
2015-04-23 10:21:20 +0200

    Fixes #42 Add breadcrumbs to User Profiles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace MyBB\Core\Http\Controllers;
 
+use DaveJamesMiller\Breadcrumbs\Manager as Breadcrumbs;
 use MyBB\Core\Database\Repositories\UserRepositoryInterface;
 use MyBB\Core\Database\Repositories\ProfileFieldGroupRepositoryInterface;
 use MyBB\Core\Database\Repositories\UserProfileFieldRepositoryInterface;
@@ -37,10 +38,12 @@ class UserController extends AbstractController
 	 *
 	 * @return \Illuminate\View\View
 	 */
-	public function profile($slug, $id, ProfileFieldGroupRepositoryInterface $profileFieldGroups)
+	public function profile($slug, $id, ProfileFieldGroupRepositoryInterface $profileFieldGroups, Breadcrumbs $breadcrumbs)
 	{
 		$user = $this->users->find($id);
 		$groups = $profileFieldGroups->getAll();
+
+		$breadcrumbs->setCurrentRoute('user.profile', $user);
 
 		return view('user.profile', [
 			'user' => $user,
