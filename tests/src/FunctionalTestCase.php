4286a087a45def38effbc746cf9534c7e61e6fdc
Will Pillar <me@willpillar.com>
2015-05-10 18:15:10 +0100

    Setting up phpunit config, test suites and coverage reports

---
+++ tests/src/FunctionalTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MyBB\Core\Test\Unit;
+
+use Illuminate\Foundation\Testing\TestCase;
+
+class FunctionalTestCase extends TestCase
+{
+
+	/**
+	 * Creates the application.
+	 *
+	 * @return \Illuminate\Foundation\Application
+	 */
+	public function createApplication()
+	{
+		$app = require __DIR__ . '/../../bootstrap/app.php';
+
+		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+		return $app;
+	}
+}
