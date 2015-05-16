c1c0002cb183855cdb68d78f3e8ac73d6c28587f
Will Pillar <me@willpillar.com>
2015-05-16 18:07:35 +0100

    Removing unused code from Admin controllers

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -12,24 +12,4 @@ use Illuminate\Routing\Controller as BaseController;
 
 class AdminController extends BaseController
 {
-
-	/*
-	|--------------------------------------------------------------------------
-	| Admin Base Controller
-	|--------------------------------------------------------------------------
-	|
-	| Base Controller for Admin Pages
-	|
-	|
-	*/
-
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		// Eventually Something will go here
-	}
 }
