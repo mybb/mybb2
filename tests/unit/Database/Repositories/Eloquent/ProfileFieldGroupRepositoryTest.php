72d4739381103172e508cbcc50c18c9f0d1c1f91
Will Pillar <me@willpillar.com>
2015-05-31 16:48:37 +0100

    Adding unit test for ProfileFieldGroupRepository

---
+++ tests/unit/Database/Repositories/Eloquent/ProfileFieldGroupRepositoryTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyBB\Core\Database\Repositories\Eloquent;
+
+use Illuminate\Support\Collection;
+use Mockery;
+
+class ProfileFieldGroupRepositoryTest extends \PHPUnit_Framework_TestCase
+{
+	public function testCanBeConstructed()
+	{
+		$model = Mockery::mock('MyBB\Core\Database\Models\ProfileFieldGroup');
+		$repository = new ProfileFieldGroupRepository($model);
+		static::assertInstanceOf('MyBB\Core\Database\Repositories\Eloquent\ProfileFieldGroupRepository', $repository);
+	}
+
+	public function testCanRetrieveProfileFieldGroupBySlug()
+	{
+		$profileFieldGroup = Mockery::mock('MyBB\Core\Database\Models\ProfileFieldGroup');
+
+		$builder = Mockery::mock('\Illuminate\Database\Eloquent\Builder');
+		$builder->shouldReceive('first')
+			->withNoArgs()
+			->andReturn($profileFieldGroup);
+
+		$model = Mockery::mock('MyBB\Core\Database\Models\ProfileFieldGroup');
+		$model->shouldReceive('where')
+			->with('slug', 'foo')
+			->andReturn($builder);
+
+		$repository = new ProfileFieldGroupRepository($model);
+		static::assertEquals($repository->getBySlug('foo'), $profileFieldGroup);
+	}
+}
