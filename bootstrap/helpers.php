e50b8787d7e45a6638bbf5a203119264820e3f54
Will Pillar <me@willpillar.com>
2015-07-26 19:48:42 +0100

    Fixing formatting and indentation for is_array_of()

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,17 +1,18 @@
 <?php
 
 if ( ! function_exists('is_array_of')) {
-    function is_array_of(array $objects, $className) {
-	    if (count($objects) === 0) {
-		    return false;
-	    }
+	function is_array_of(array $objects, $className)
+	{
+		if (count($objects) === 0) {
+			return false;
+		}
 
-        foreach ($objects as $object) {
-            if (! is_a($object, $className)) {
-                return false;
-            }
-        }
+		foreach ($objects as $object) {
+			if (! is_a($object, $className)) {
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
