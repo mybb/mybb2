efe28c554b10cf87f0dae2921ac61925a6d0d2e6
Euan Torano <euantorano@gmail.com>
2016-03-17 20:13:47 +0000

    Move clear fix to a mixing, add _mixins.scss.
    Move towards elixir and NPM to simplify the management of static
    resources for #197 and #196.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,13 @@
 "use strict";
 
 const gulp = require("gulp"),
+    del = require("del"),
+    elixir = require('laravel-elixir'),
     changed = require("gulp-changed"),
     imagemin = require("gulp-imagemin"),
-    concat = require("gulp-concat"),
-    uglify = require("gulp-uglify"),
-    stripDebug = require("gulp-strip-debug"),
-    sass = require("gulp-sass"),
-    autoprefixer = require("gulp-autoprefixer"),
-    nano = require('gulp-cssnano'),
-    rename = require("gulp-rename"),
-    del = require("del"),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    browserify = require("browserify"),
-    sourcemaps = require('gulp-sourcemaps');
+    pngquant = require('imagemin-pngquant');
 
-var paths = {
+const paths = {
     bower: "./bower_components",
     dist: "./public/assets",
     js: {
@@ -76,7 +67,7 @@ const css = [
     paths.bower + "/normalize.css/normalize.css",
     paths.bower + "/fontawesome/scss/font-awesome.scss",
     paths.bower + "/dropit/dropit.css",
-    paths.bower + "/jquery-dropdown/jquery.dropdown.css",
+    paths.bower + "/jquery-dropdown/jquery.dropdown.min.css",
     paths.bower + "/datetimepicker/jquery.datetimepicker.css",
     paths.css.src + "/main.scss"
 ];
@@ -85,143 +76,109 @@ const admin_css = [
     paths.bower + "/normalize.css/normalize.css",
     paths.bower + "/fontawesome/scss/font-awesome.scss",
     paths.bower + "/dropit/dropit.css",
-    paths.bower + "/jquery-dropdown/jquery.dropdown.css",
+    paths.bower + "/jquery-dropdown/jquery.dropdown.min.css",
     paths.bower + "/datetimepicker/jquery.datetimepicker.css",
     paths.css.src + "/admin.scss"
 ];
 
-gulp.task("default", ["images", "vendor_scripts", "scripts", "styles", "admin_styles", "rtl_styles", "admin_rtl_styles", "fonts"]);
-
-gulp.task("clean", function (cb) {
-    del(
-        [
-            paths.js.dest + "/**",
-            paths.css.dest + "/**",
-            paths.images.dest + "/**",
-            paths.fonts.dest + "/**"
-        ]
-        , cb);
+gulp.task("clean", (cb) => {
+    del([
+        paths.js.dest + "/**",
+        paths.css.dest + "/**",
+        paths.images.dest + "/**",
+        paths.fonts.dest + "/**"
+    ], cb);
 });
 
-gulp.task("watch", ["default"], function () {
-    gulp.watch("./bower_components/**/*.js", ["vendor_scripts"]);
-    gulp.watch(paths.js.src + "/**/*.js", ["scripts"]);
-    gulp.watch(paths.images.src + "/**/*", ["images"]);
-    gulp.watch(paths.css.src + "/**/*.scss", ["styles"]);
-    gulp.watch(paths.css.src + "/**/*.scss", ["rtl_styles"]);
-    gulp.watch(paths.css.src + "/**/*.scss", ["admin_styles"]);
-    gulp.watch(paths.css.src + "/**/*.scss", ["admin_rtl_styles"]);
-    gulp.watch(paths.fonts.src + "/**/*", ["fonts"]);
-});
-
-gulp.task("images", function () {
+gulp.task("images", (cb) => {
     return gulp.src(paths.images.src + "/*")
         .pipe(changed(paths.images.dest))
-        .pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [
+                {removeViewBox: false},
+                {cleanupIDs: false}
+            ],
+            use: [pngquant()]
+        }))
         .pipe(gulp.dest(paths.images.dest));
 });
 
-gulp.task("vendor_scripts", function () {
-    return gulp.src(vendor_scripts)
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(concat("vendor.js"))
-        .pipe(gulp.dest(paths.js.dest + "/"))
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(rename({
-            suffix: ".min"
-        }))
-        .pipe(gulp.dest(paths.js.dest + "/"));
+gulp.task("fonts", (cb) => {
+    return gulp.src(paths.bower + "/fontawesome/fonts/**.*")
+        .pipe(gulp.dest(paths.fonts.dest));
 });
 
-gulp.task("scripts", function () {
-    console.log(paths.js.dest);
-    return gulp.src(scripts)
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(concat("main.js"))
-        .pipe(gulp.dest(paths.js.dest + "/"))
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(rename({
-            suffix: ".min"
-        }))
-        .pipe(gulp.dest(paths.js.dest + "/"));
+elixir((mix) => {
+    mix.browserSync();
+
+    mix.task("clean")
+        .sass(css, paths.css.dest) // Main CSS
+        .sass(admin_css, paths.css.dest) // Admin CSS
+        .browserify(vendor_scripts, paths.js.dest + "/vendor.js") // Vendor JS (jQuery, etc.)
+        .browserify(scripts, paths.js.dest + "/main.js"); // Main app JS
 });
 
-gulp.task("styles", function () {
-    return gulp.src(css)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: [
-                "./app/bower_components"
-            ]
-        }))
-        .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
-        .pipe(concat("main.css"))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest))
-        .pipe(rename({suffix: ".min"}))
-        .pipe(nano())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest));
-});
+// const
+//     concat = require("gulp-concat"),
+//     uglify = require("gulp-uglify"),
+//     stripDebug = require("gulp-strip-debug"),
+//     sass = require("gulp-sass"),
+//     autoprefixer = require("gulp-autoprefixer"),
+//     nano = require('gulp-cssnano'),
+//     rename = require("gulp-rename"),
+//     source = require('vinyl-source-stream'),
+//     buffer = require('vinyl-buffer'),
+//     browserify = require("browserify"),
+//     sourcemaps = require('gulp-sourcemaps');
+//
+// gulp.task("default", ["images", "vendor_scripts", "scripts", "styles", "admin_styles", "rtl_styles", "admin_rtl_styles", "fonts"]);
+//
+// gulp.task("watch", ["default"], function () {
+//     gulp.watch("./bower_components/**/*.js", ["vendor_scripts"]);
+//     gulp.watch(paths.js.src + "/**/*.js", ["scripts"]);
+//     gulp.watch(paths.images.src + "/**/*", ["images"]);
+//     gulp.watch(paths.css.src + "/**/*.scss", ["styles"]);
+//     gulp.watch(paths.css.src + "/**/*.scss", ["rtl_styles"]);
+//     gulp.watch(paths.css.src + "/**/*.scss", ["admin_styles"]);
+//     gulp.watch(paths.css.src + "/**/*.scss", ["admin_rtl_styles"]);
+//     gulp.watch(paths.fonts.src + "/**/*", ["fonts"]);
+// });
 
-gulp.task("admin_styles", function () {
-    return gulp.src(admin_css)
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: [
-                "./app/bower_components"
-            ]
-        }))
-        .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
-        .pipe(concat("admin.css"))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest))
-        .pipe(rename({suffix: ".min"}))
-        .pipe(nano())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest));
-});
 
-gulp.task("rtl_styles", function () {
-    return gulp.src([paths.bower + "/normalize.css/normalize.css", paths.bower + "/fontawesome/scss/font-awesome.scss", paths.css.src + "/rtl.scss"])
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: [
-                "./app/bower_components"
-            ]
-        }))
-        .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
-        .pipe(concat("rtl.css"))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest))
-        .pipe(rename({suffix: ".min"}))
-        .pipe(nano())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest));
-});
 
-gulp.task("admin_rtl_styles", function () {
-    return gulp.src([paths.bower + "/normalize.css/normalize.css", paths.bower + "/fontawesome/scss/font-awesome.scss", paths.css.src + "/admin.rtl.scss"])
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: [
-                "./app/bower_components"
-            ]
-        }))
-        .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
-        .pipe(concat("admin.rtl.css"))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest))
-        .pipe(rename({suffix: ".min"}))
-        .pipe(nano())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(paths.css.dest));
-});
-
-gulp.task("fonts", function () {
-    return gulp.src(paths.bower + "/fontawesome/fonts/**.*").pipe(gulp.dest(paths.fonts.dest));
-});
+// gulp.task("rtl_styles", function () {
+//     return gulp.src([paths.bower + "/normalize.css/normalize.css", paths.bower + "/fontawesome/scss/font-awesome.scss", paths.css.src + "/rtl.scss"])
+//         .pipe(sourcemaps.init())
+//         .pipe(sass({
+//             includePaths: [
+//                 "./app/bower_components"
+//             ]
+//         }))
+//         .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
+//         .pipe(concat("rtl.css"))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.css.dest))
+//         .pipe(rename({suffix: ".min"}))
+//         .pipe(nano())
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.css.dest));
+// });
+//
+// gulp.task("admin_rtl_styles", function () {
+//     return gulp.src([paths.bower + "/normalize.css/normalize.css", paths.bower + "/fontawesome/scss/font-awesome.scss", paths.css.src + "/admin.rtl.scss"])
+//         .pipe(sourcemaps.init())
+//         .pipe(sass({
+//             includePaths: [
+//                 "./app/bower_components"
+//             ]
+//         }))
+//         .pipe(autoprefixer("last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"))
+//         .pipe(concat("admin.rtl.css"))
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.css.dest))
+//         .pipe(rename({suffix: ".min"}))
+//         .pipe(nano())
+//         .pipe(sourcemaps.write('.'))
+//         .pipe(gulp.dest(paths.css.dest));
+// });
