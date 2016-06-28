9ceeef612dce3115b616bbb97827a99a45d87296
Mateusz <mateosl111@gmail.com>
2016-06-28 10:48:20 +0200

    Fix #227 and follow style standard

--- app/Database/Repositories/UserRepositoryInterface.php
+++ app/Database/Repositories/UserRepositoryInterface.php
@@ -12,53 +12,53 @@ namespace MyBB\Core\Database\Repositories;
 
 interface UserRepositoryInterface
 {
-	/**
-	 * Get all users.
-	 *
-	 * @param string $sortBy
-	 * @param string $sortDir
-	 * @param int    $perPage
-	 *
-	 * @return mixed
-	 */
-	public function all($sortBy = 'created_at', $sortDir = 'desc', $perPage = 10);
+    /**
+     * Get all users.
+     *
+     * @param string $sortBy
+     * @param string $sortDir
+     * @param int $perPage
+     *
+     * @return mixed
+     */
+    public function all($sortBy = 'created_at', $sortDir = 'desc', $perPage = 10);
 
-	/**
-	 * Get all users active in the last x minutes
-	 *
-	 * @param int    $minutes  The number of minutes which are considered as "online time"
-	 * @param string $orderBy
-	 * @param string $orderDir
-	 * @param int    $num      The number of users to return. Set to 0 to get all users
-	 *
-	 * @return mixed
-	 */
-	public function online($minutes = 15, $orderBy = 'last_visit', $orderDir = 'desc', $num = 20);
+    /**
+     * Get all users active in the last x minutes
+     *
+     * @param int $minutes The number of minutes which are considered as "online time"
+     * @param string $orderBy
+     * @param string $orderDir
+     * @param int $num     The number of users to return. Set to 0 to get all users
+     *
+     * @return mixed
+     */
+    public function online($minutes = 15, $orderBy = 'last_visit', $orderDir = 'desc', $num = 20);
 
-	/**
-	 * Find a single user by ID.
-	 *
-	 * @param int $id The ID of the user to find.
-	 *
-	 * @return mixed
-	 */
-	public function find($id = 0);
+    /**
+     * Find a single user by ID.
+     *
+     * @param int $id The ID of the user to find.
+     *
+     * @return mixed
+     */
+    public function find($id = 0);
 
-	/**
-	 * Find a single user by its username.
-	 *
-	 * @param string $username The username of the user. Eg: 'admin'.
-	 *
-	 * @return mixed
-	 */
-	public function findByUsername($username = '');
+    /**
+     * Find a single user by its username.
+     *
+     * @param string $username The username of the user. Eg: 'admin'.
+     *
+     * @return mixed
+     */
+    public function findByUsername($username = '');
 
-	/**
-	 * Create a new user
-	 *
-	 * @param array $details Details about the user.
-	 *
-	 * @return User
-	 */
-	public function create(array $details = []);
+    /**
+     * Create a new user
+     *
+     * @param array $details Details about the user.
+     *
+     * @return User
+     */
+    public function create(array $details = []);
 }
--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,218 +21,218 @@ use MyBB\Core\Http\Controllers\AbstractController as Controller;
 class AuthController extends Controller
 {
 
-	/*
-	|--------------------------------------------------------------------------
-	| MyBB Authentication Manager
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. Trait methods have been changes to accomodate
-	| MyBB Templates
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | MyBB Authentication Manager
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users, as well as the
+    | authentication of existing users. Trait methods have been changes to accomodate
+    | MyBB Templates
+    |
+    */
 
-	use AuthenticatesAndRegistersUsers;
+    use AuthenticatesAndRegistersUsers;
 
-	/**
-	 * @var AuthManager $auth
-	 */
-	private $auth;
+    /**
+     * @var AuthManager $auth
+     */
+    private $auth;
 
-	/**
-	 * @var Breadcrumbs $breadcrumbs
-	 */
-	private $breadcrumbs;
+    /**
+     * @var Breadcrumbs $breadcrumbs
+     */
+    private $breadcrumbs;
 
-	/**
-	 * @var Factory $validator
-	 */
-	private $validator;
+    /**
+     * @var Factory $validator
+     */
+    private $validator;
 
-	/**
-	 * @var Request $request
-	 */
-	private $request;
+    /**
+     * @var Request $request
+     */
+    private $request;
 
-	/**
-	 * @var RoleRepositoryInterface
-	 */
-	private $roleRepository;
+    /**
+     * @var RoleRepositoryInterface
+     */
+    private $roleRepository;
 
-	/**
-	 * @var UserRepositoryInterface
-	 */
-	private $userRepository;
+    /**
+     * @var UserRepositoryInterface
+     */
+    private $userRepository;
 
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param AuthManager $auth
-	 * @param Breadcrumbs $breadcrumbs
-	 * @param Factory $validator
-	 * @param Request $request
-	 * @param RoleRepositoryInterface $roleRepository
-	 * @param UserRepositoryInterface $userRepository
-	 */
-	public function __construct(AuthManager $auth, Breadcrumbs $breadcrumbs, Factory $validator, Request $request, RoleRepositoryInterface $roleRepository, UserRepositoryInterface $userRepository)
-	{
-		$this->auth = $auth;
-		$this->breadcrumbs = $breadcrumbs;
-		$this->validator = $validator;
-		$this->request = $request;
-		$this->userRepository = $userRepository;
-		$this->roleRepository = $roleRepository;
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @param AuthManager $auth
+     * @param Breadcrumbs $breadcrumbs
+     * @param Factory $validator
+     * @param Request $request
+     * @param RoleRepositoryInterface $roleRepository
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(AuthManager $auth, Breadcrumbs $breadcrumbs, Factory $validator, Request $request, RoleRepositoryInterface $roleRepository, UserRepositoryInterface $userRepository)
+    {
+        $this->auth = $auth;
+        $this->breadcrumbs = $breadcrumbs;
+        $this->validator = $validator;
+        $this->request = $request;
+        $this->userRepository = $userRepository;
+        $this->roleRepository = $roleRepository;
 
-		$this->middleware('guest', ['except' => 'getLogout']);
-	}
-
-
-	/**
-	 * Show the application registration form.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function getSignup()
-	{
-		$this->breadcrumbs->setCurrentRoute('auth.signup');
-
-		return view('member.signup');
-	}
-
-	/**
-	 * Handle a registration request for the application.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function postSignup()
-	{
-		$this->failedValidationRedirect = url('auth/signup');
-
-		$captcha = $this->checkCaptcha();
-		if ($captcha !== true) {
-			return $captcha;
-		}
-
-		$validator = $this->validator($this->request->all());
-
-		if ($validator->fails()) {
-			$this->throwValidationException(
-				$this->request,
-				$validator
-			);
-		}
-
-		$this->auth->login($this->create($this->request->all()));
-
-		return redirect($this->redirectPath());
-	}
-
-	/**
-	 * Show the application login form.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function getLogin()
-	{
-		$this->breadcrumbs->setCurrentRoute('auth.login');
-
-		return view('member.login');
-	}
-
-	/**
-	 * Handle a login request to the application.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function postLogin()
-	{
-		$this->failedValidationRedirect = url('auth/login');
-
-		$this->validate($this->request, [
-			'username' => 'required',
-			'password' => 'required',
-		]);
-
-		$credentials = $this->request->only('username', 'password');
-
-		if ($this->auth->attempt(
-			['name' => $credentials['username'], 'password' => $credentials['password']],
-			$this->request->input('remember_me')
-		)
-		) {
-			return redirect()->intended($this->redirectPath());
-		}
-
-		return redirect('/auth/login')
-			->withInput($this->request->only('username', 'remember_me'))
-			->withErrors([
-				'username' => trans('member.invalidCredentials'),
-			]);
-	}
-
-	/**
-	 * Log the user out of the application.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function getLogout()
-	{
-		$this->auth->logout();
-		$cookie = cookie()->forget('quotes'); // Remove cookies
-
-		return redirect('/')->withCookie($cookie);
-	}
-
-	/**
-	 * Get the post register / login redirect path.
-	 *
-	 * @return string
-	 */
-	public function redirectPath()
-	{
-		$redirectUrl = $this->request->input('url');
-
-		if (!empty($redirectUrl)) {
-			return $redirectUrl;
-		}
-
-		return '/';
-	}
+        $this->middleware('guest', ['except' => 'getLogout']);
+    }
 
 
-	/**
-	 * Get a validator for an incoming registration request.
-	 *
-	 * @param  array $data
-	 *
-	 * @return \Illuminate\Contracts\Validation\Validator
-	 */
-	protected function validator(array $data)
-	{
-		return $this->validator->make($data, [
-			'name'     => 'required|max:255|unique:users',
-			'email'    => 'required|email|max:255|unique:users',
-			'password' => 'required|confirmed|min:6',
-		]);
-	}
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getSignup()
+    {
+        $this->breadcrumbs->setCurrentRoute('auth.signup');
 
-	/**
-	 * Create a new user instance after a valid registration.
-	 *
-	 * @param  array $data
-	 *
-	 * @return User
-	 */
-	protected function create(array $data)
-	{
-		$user = $this->userRepository->create([
-			'name'     => $data['name'],
-			'email'    => $data['email'],
-			'password' => bcrypt($data['password']),
-		]);
+        return view('member.signup');
+    }
 
-		$user->roles()->attach($this->roleRepository->findIdBySlug('user'), ['is_display' => true]);
+    /**
+     * Handle a registration request for the application.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function postSignup()
+    {
+        $this->failedValidationRedirect = url('auth/signup');
 
-		return $user;
-	}
+        $captcha = $this->checkCaptcha();
+        if ($captcha !== true) {
+            return $captcha;
+        }
+
+        $validator = $this->validator($this->request->all());
+
+        if ($validator->fails()) {
+            $this->throwValidationException(
+                $this->request,
+                $validator
+            );
+        }
+
+        $this->auth->login($this->create($this->request->all()));
+
+        return redirect($this->redirectPath());
+    }
+
+    /**
+     * Show the application login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLogin()
+    {
+        $this->breadcrumbs->setCurrentRoute('auth.login');
+
+        return view('member.login');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function postLogin()
+    {
+        $this->failedValidationRedirect = url('auth/login');
+
+        $this->validate($this->request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $this->request->only('username', 'password');
+
+        if ($this->auth->attempt(
+            ['name' => $credentials['username'], 'password' => $credentials['password']],
+            $this->request->input('remember_me')
+        )
+        ) {
+            return redirect()->intended($this->redirectPath());
+        }
+
+        return redirect('/auth/login')
+            ->withInput($this->request->only('username', 'remember_me'))
+            ->withErrors([
+                'username' => trans('member.invalidCredentials'),
+            ]);
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getLogout()
+    {
+        $this->auth->logout();
+        $cookie = cookie()->forget('quotes'); // Remove cookies
+
+        return redirect('/')->withCookie($cookie);
+    }
+
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        $redirectUrl = $this->request->input('url');
+
+        if (!empty($redirectUrl)) {
+            return $redirectUrl;
+        }
+
+        return '/';
+    }
+
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array $data
+     *
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return $this->validator->make($data, [
+            'name'     => 'required|max:255|unique:users',
+            'email'    => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array $data
+     *
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        $user = $this->userRepository->create([
+            'name'     => $data['name'],
+            'email'    => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $user->roles()->attach($this->roleRepository->findIdBySlug('user'), ['is_display' => true]);
+
+        return $user;
+    }
 }
