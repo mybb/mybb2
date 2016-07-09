4b3421be713b6727c69a50f7b9c9fd01f5881475
Euan Torano <euantorano@gmail.com>
2016-07-09 17:13:35 +0100

    More work on Gulp
    Handling images, fonts and SASS correctly, just need to do scripts.
    
    In the future, scripts will use ES6 modules, so more changes will be needed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 "use strict";
 
-const elixir = require("laravel-elixir");
+const
+    gulp = require("gulp"),
+    imagemin = require('gulp-imagemin'),
+    elixir = require("laravel-elixir");
 
 elixir.config.sourcemaps = true;
 
+gulp.task("images", () => {
+    gulp.src("resources/assets/images/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("public/assets/images"));
+});
 
 elixir(mix => {
-    mix.sass("main.scss", "public/assets/css/main.css");
+    mix
+        .sass("main.scss", "public/assets/css/main.css")
+        .sass("admin.scss", "public/assets/css/admin.css")
+        .sass("rtl.scss", "public/assets/css/rtl.css")
+        .sass("admin.rtl.scss", "public/assets/css/admin_rtl.css")
+        .copy("node_modules/font-awesome/fonts", "public/assets/fonts")
+        .task("images", "resources/assets/images/**/*");
 });
