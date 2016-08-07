7a819e6de65f37bbde70249408fc96743a5ce5e7
Euan Torano <euantorano@gmail.com>
2016-08-07 14:13:22 +0100

    Working on initial widget system.

---
+++ app/Widgets/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author    MyBB Group
+ * @version   2.0.0
+ * @package   mybb/core
+ * @license   http://www.mybb.com/licenses/bsd3 BSD-3
+ */
+
+namespace MyBB\Core\Widgets;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $reg = new Registry($this->app);
+        $reg->registerWidget(Widgets\RenderViewWidget::getName(), Widgets\RenderViewWidget::class);
+
+        $this->app->instance(Registry::class, $reg);
+    }
+}
---
+++ app/Widgets/Twig/Widget.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @author    MyBB Group
+ * @version   2.0.0
+ * @package   mybb/core
+ * @license   http://www.mybb.com/licenses/bsd3 BSD-3
+ */
+
+namespace MyBB\Core\Widgets\Twig;
+
+
+use MyBB\Core\Widgets\Registry;
+
+class Widget extends \Twig_Extension
+{
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @param Registry $registry The widget registry.
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'MyBB_Widgets_Twig_Extensions_Widget';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('render_widget', [$this->registry, 'renderWidget'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('widget_position', [$this->registry, 'renderWidgetsForPosition'], ['is_safe' => ['html']]),
+        ];
+    }
+}
