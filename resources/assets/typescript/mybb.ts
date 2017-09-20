4596c0a657388cb34f46915483aa04e92057db27
Eric <ericj1992@gmail.com>
2017-09-19 20:49:03 -0400

    Added post Typescript
    Added post module and utils module.

--- resources/assets/typescript/mybb.ts
+++ resources/assets/typescript/mybb.ts
@@ -1,7 +1,10 @@
 import Cookie from "./cookie";
 import Spinner from "./spinner";
+import Post from "./post";
 
-window.mybb = {
+(<any>window).mybb = {
+    Lang: (<any>window).Lang || {}, // TODO: Make an ES6 module
     cookie: new Cookie(),
     spinner: new Spinner(),
+    post: new Post()
 };
